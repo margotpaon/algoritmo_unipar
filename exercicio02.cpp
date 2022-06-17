341ec8d36925eea068bf47196adc5b5bcf7b6e68
Margot <margot.paon@gmail.com>
2022-06-17 17:35:36 -0300

    Exercicio peso ideal

---
+++ exercicio02.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+    char gender;
+    double altura, peso_ideal = 0;
+    printf("Digite seu genero M para masculino ou F para feminino \n");
+    scanf("%c", &gender);
+    printf("Digite sua altura: \n");
+    scanf("%lf", &altura);
+    
+    if(gender=='M'||gender=='m'){
+        peso_ideal = (450.7 * altura) / 10;
+        printf("O seu peso ideal deve ser %lf", peso_ideal);
+    }else if (gender=='F'||gender=='f'){
+        peso_ideal = (400.7 * altura) / 10;
+        printf("O seu peso ideal deve ser %lf", peso_ideal);
+    }
+
+
+    return 0;
+}
